20097854e264e6294897671c87bca0c298ae2398
Spencer Gibb <spencer@gibb.us>
2015-09-23 09:38:37 -0600

    first commit

---
+++ src/main/java/org/springframework/reacvtive/web/client/SpringRestClientApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.reacvtive.web.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientApplication.class, args);
+    }
+}
